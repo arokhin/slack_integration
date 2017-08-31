835c5f30ba61839b0b06a93fa0b9dee286753bf5
artem <artem.rokhin@jetbrains.com>
2017-08-31 16:52:21 +0300

    Minor polishing

--- webhook.py
+++ webhook.py
@@ -4,11 +4,13 @@ import requests
 import os
 
 slack_url = os.getenv('SLACK_WEBHOOK_URL')
+upsource_url = os.getenv('UPSOURCE_BASE_URL')
+botname = "Upsource BOT"
 
 
 def find_json_key(key, data):
     results = []
-    data2 = json.dumps(data)
+    in_json = json.dumps(data)
 
     def _decode_dict(a_dict):
         try:
@@ -17,7 +19,7 @@ def find_json_key(key, data):
             pass
         return a_dict
 
-    json.loads(data2, object_hook=_decode_dict)
+    json.loads(in_json, object_hook=_decode_dict)
     return results[0]
 
 app = Flask(__name__)
@@ -31,14 +33,13 @@ def index():
         reviewid = find_json_key('reviewId', file)
         print(reviewid)
 
-        botname = "Upsource BOT"
-        text = "Review <https://url_to_my_upsource|%s> has been created" % reviewid
-
-        url = slack_url
-        data = {'text': text, 'username': botname}
+        text = "Review <%s|%s> has been created" % (upsource_url, reviewid)
+        data = {'channel': '#general', 'text': text, 'username': botname}
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        requests.post(url, data=json.dumps(data), headers=headers)
 
+        requests.post(slack_url, data=json.dumps(data), headers=headers)
+
+        print(data)
     else:
         print('Something else has happened')
     return "OK"
